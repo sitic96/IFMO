9ff4f366f1b81a66923f7ad48e5b99e2c6774ee9
Sitora <redastify@gmail.com>
2016-05-27 18:12:28 +0300

    Лабораторные с первого курса

---
+++ Course_1/BST/BST/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int j = 0; j < 15; j++)
+            {
+                Tree<int> tree = new Tree<int>();
+                // var j = 15;
+                int lastInsertElement = new int();
+                using (StreamReader sr = new StreamReader((j + 1) + ".txt"))
+                {
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(new char[] { ' ' });
+                        for (int i = 0; i < line.Length; i++)
+                        {
+                            if (line[i] == "")
+                            {
+                                break;
+                            }
+                            int element = Convert.ToInt32(line[i]);
+                            tree.Insert(element);
+                            lastInsertElement = element;
+                        }
+                    }
+                }
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                lastInsertElement = 7;
+                bool isFind = tree.Search(lastInsertElement);
+                sw.Stop();
+                Console.WriteLine(sw.ElapsedTicks);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ Course_1/BST/BST/Tree.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST
+{
+    class Tree<T> where T : IComparable<T>
+    {
+        private T value;
+        private Tree<T> left;
+        private Tree<T> right;
+
+        // вставка
+        public void Insert(T value)
+        {
+            if (this.value.Equals(default(T)))
+                this.value = value;
+            else
+            {
+                if (this.value.CompareTo(value) == 1)
+                {
+                    if (left == null)
+                        this.left = new Tree<T>();
+                    left.Insert(value);
+                }
+                else
+                {
+                    if (right == null)
+                        this.right = new Tree<T>();
+                    right.Insert(value);
+                }
+            }
+        }
+        // поиск
+        public bool Search(T value)
+        {
+            if (this.value.Equals(value))
+                return true;
+
+            else if (this.value.CompareTo(value) == 1)
+            {
+                if (left != null)
+                    return this.left.Search(value);
+                else
+                    return false;
+            }
+            else
+            {
+                if (right != null)
+                    return this.right.Search(value);
+                else
+                    return false;
+            }
+        }
+    }
+}
