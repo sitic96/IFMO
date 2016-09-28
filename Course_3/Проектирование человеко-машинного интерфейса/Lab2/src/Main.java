5e270c1c01f6cd426605c25420707950f3708359
Sitora <redastify@gmail.com>
2016-09-28 22:54:25 +0400

    Update Main.java

--- Course_3/Проектирование человеко-машинного интерфейса/Lab2/src/Main.java
+++ Course_3/Проектирование человеко-машинного интерфейса/Lab2/src/Main.java
@@ -2,8 +2,6 @@ import javax.swing.*;
 import java.awt.image.BufferedImage;
 
 public class Main extends JFrame{
-    private BufferedImage img = null;
-
     public static void main(String[] args) {
         Panel panel = new Panel("src/images/rainbow.bmp");
     }
