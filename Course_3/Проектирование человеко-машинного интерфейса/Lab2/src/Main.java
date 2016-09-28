3e0c1f052ad6be36636033e6b0c72b71d140c932
Sitora <redastify@gmail.com>
2016-09-28 21:52:55 +0300

    Вторая лаба по чми (отличие от первой - метод doSecondWork)

---
+++ Course_3/Проектирование человеко-машинного интерфейса/Lab2/src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
+public class Main extends JFrame{
+    private BufferedImage img = null;
+
+    public static void main(String[] args) {
+        Panel panel = new Panel("src/images/rainbow.bmp");
+    }
+}
