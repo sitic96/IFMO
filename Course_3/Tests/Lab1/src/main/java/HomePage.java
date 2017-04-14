693f4354420b3685930239d42fdfc430bd502094
Sitora <redastify@gmail.com>
2017-04-15 00:01:05 +0300

    Первые тесты

---
+++ Course_3/Tests/Lab1/src/main/java/HomePage.java
@@ -0,0 +1,18 @@
+package main.java;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by sitora on 10.04.17.
+ */
+public class HomePage {
+    // the tabs
+    public static final String LOC_LNK_PROJECTSTAB = "li#menu_projects a[href$='projects/']";
+    public static final String LOC_LNK_DOWNLOADTAB = "li#menu_download a[href$='download/']";
+    public static final By LOC_LNK_DOCUMENTATIONTAB = By.xpath("//li[@id='menu_documentation']/a[contains(@href, 'docs/')]");
+    public static final String LOC_LNK_SUPPORTTAB = "li#menu_support a[href$='support/']";
+    public static final String LOC_LNK_ABOUTTAB = "li#menu_about a[href$='about/']";
+
+    // download link
+    public static final By LOC_LNK_DOWNLOADSELENIUM = By.linkText("Download Selenium");
+}
