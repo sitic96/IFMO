bcdfa196bc3916555226353ec5a13a8f2916bd58
Sitora <redastify@gmail.com>
2017-06-24 13:47:41 +0300

    Добавлены тесты, сдано

--- Course_3/Tests/Tumblr/tests/CreatePost.java
+++ Course_3/Tests/Tumblr/tests/CreatePost.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
-
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CreatePost extends BasicTest {
     private boolean isSetUp = false;
@@ -20,7 +19,7 @@ public class CreatePost extends BasicTest {
     }
 
     @Test
-    public void createPost() {
+    public void createPost() throws InterruptedException {
         new WebDriverWait(driver, 15).until(
                 ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[class='compose-button']"))).click();
 
@@ -35,12 +34,67 @@ public class CreatePost extends BasicTest {
 
         driver.findElement(By.cssSelector("input[type='file']")).sendKeys("/Users/mac/Desktop/w_eb191985.jpg");
 
-        // driver.findElement(By.cssSelector("div[class='control add-inline-embed']")).click();
-        //driver.findElement(By.cssSelector("div[class='editor editor-richtext']")).sendKeys("https://www.youtube.com/watch?v=Z8lUW9SKquY");
 
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[class='button-area create_post_button']"))).click();
+
+        //new WebDriverWait(driver, 50).until(
+          //      ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[class='button-area csdfeate_post_button']"))).click();
+
+    }
+
+    @Test
+    public void createTextOnly() {
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("new_post_label_text"))).click();
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='editor editor-plaintext']"))).sendKeys("My post");
+
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='editor editor-richtext']"))).sendKeys("Hello world!");
+        driver.findElement(By.cssSelector("button[class='tx-button']")).click();
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[class='ui_button btn_1 chrome blue']"))).click();
+
+    }
+
+    @Test
+    public void createQuote() {
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("new_post_label_quote"))).click();
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='editor editor-plaintext']"))).sendKeys("I HATE TESTING");
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='editor editor-richtext']"))).sendKeys("KALININ DENIS P3317");
         driver.findElement(By.cssSelector("button[class='tx-button']")).click();
         new WebDriverWait(driver, 15).until(
                 ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[class='ui_button btn_1 chrome blue']"))).click();
     }
 
+    @Test
+    public void createLink() {
+        driver.findElement(By.id("new_post_label_link")).click();
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='editor editor-plaintext']"))).sendKeys("iam.ifmo.ru");
+        driver.findElement(By.cssSelector("button[class='tx-button']")).click();
+    }
+
+    @Test
+    public void createChat() {
+        driver.findElement(By.id("new_post_label_chat")).click();
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[class='tx-button']"))).click();
+    }
+
+    @Test
+    public void createAudio() {
+        driver.findElement(By.id("new_post_label_audio")).click();
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='editor editor-plaintext']"))).sendKeys("Where is the love");
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[data-href='spotify:track:3CNqo3gYrfexdrtjFmC9he']"))).click();
+        driver.findElement(By.cssSelector("button[class='tx-button']")).click();
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[class='ui_button btn_1 chrome blue']"))).click();
+    }
 }
--- Course_3/Tests/Tumblr/tests/SearchTests.java
+++ Course_3/Tests/Tumblr/tests/SearchTests.java
@@ -26,33 +26,46 @@ public class SearchTests extends BasicTest {
     }
 
     @Test
-    public void topPanelTests() {
+    public void anotherSearch() {
+        new WebDriverWait(driver, 10).until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("search_query"))).sendKeys("Glee" + Keys.ENTER);
+    }
+
+    /**
+     * Don't run without search
+     */
+    @Test
+    public void topPanelTests() throws InterruptedException {
         //Recommended
         new WebDriverWait(driver, 15).until(
                 ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[data-content-type='recommended-for-you']"))).click();
-
+        wait2Seconds();
         //Trending
         new WebDriverWait(driver, 15).until(
                 ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[data-content-type='trending']"))).click();
+        wait2Seconds();
+        //Staff picks
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[data-content-type='staff-picks']"))).click();
+        wait2Seconds();
+        //Text
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[data-content-type='text']"))).click();
+        wait2Seconds();
+        //Photos
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[data-content-type='photos']"))).click();
+        wait2Seconds();
+        //Gifs
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[data-content-type='gifs']"))).click();
+        wait2Seconds();
+        //Quotes
+        new WebDriverWait(driver, 15).until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[data-content-type='quotes']"))).click();
+    }
 
-//        //Staff picks
-//        new WebDriverWait(driver, 15).until(
-//                ExpectedConditions.visibilityOfElementLocated(By.xpath("a[data-content-type='staff-picks']"))).click();
-//
-//        //Text
-//        new WebDriverWait(driver, 15).until(
-//                ExpectedConditions.visibilityOfElementLocated(By.xpath("a[data-content-type='text']"))).click();
-//
-//        //Photos
-//        new WebDriverWait(driver, 15).until(
-//                ExpectedConditions.visibilityOfElementLocated(By.xpath("a[data-content-type='photos']"))).click();
-//
-//        //Gifs
-//        new WebDriverWait(driver, 15).until(
-//                ExpectedConditions.visibilityOfElementLocated(By.xpath("a[data-content-type='gifs']"))).click();
-//
-//        //Quotes
-//        new WebDriverWait(driver, 15).until(
-//                ExpectedConditions.visibilityOfElementLocated(By.xpath("a[data-content-type='quotes']"))).click();
+    public void wait2Seconds() throws InterruptedException {
+        Thread.sleep(2000);
     }
 }
