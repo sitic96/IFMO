6dad7ccdaf0dde662b1fc1d0ba456b2de3eb2fa3
Sitora <redastify@gmail.com>
2017-09-21 23:37:50 +0300

    Доделанный вариант

--- Course_4/DB/lab1.sql
+++ Course_4/DB/lab1.sql
@@ -1,7 +1,9 @@
 DECLARE
-  input_line VARCHAR(20);
+  input_line  VARCHAR(20);
+  word_length NUMBER;
 BEGIN
   input_line := :k;
+  word_length := :j;
   dbms_output.put_line('Последовательность символов: ' || input_line);
   dbms_output.put_line('Длина: ' || length(input_line));
   dbms_output.put_line('No. Ключевое слово');
@@ -12,7 +14,7 @@ BEGIN
      ROWNUM,
      keyword
    FROM v$reserved_words
-   WHERE LENGTH(keyword) = 6 AND upper(keyword) LIKE upper(input_line || '%'))
+   WHERE LENGTH(keyword) = word_length AND upper(keyword) LIKE upper(input_line || '%') AND reserved = 'Y')
   LOOP
     dbms_output.put_line(RPAD(row.rownum, 3) || ' ' || RPAD(row.keyword, 25));
   END LOOP;
