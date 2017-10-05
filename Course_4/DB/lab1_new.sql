d824368bbf59893a6d264cc9fb307083f62feff8
Sitora <redastify@gmail.com>
2017-10-05 20:34:04 +0300

    Работающий на хелиос вариант

---
+++ Course_4/DB/lab1_new.sql
@@ -0,0 +1,28 @@
+﻿set serveroutput on
+accept k prompt 'length: '
+accept l prompt 'line: '
+DECLARE
+  k NUMBER;
+  l VARCHAR(20);
+  input_line  VARCHAR(20);
+  word_length NUMBER;
+BEGIN
+  input_line := &l;
+  word_length:= &k;
+  dbms_output.put_line('Последовательность символов: ' || input_line);
+  dbms_output.put_line('Длина: ' || length(input_line));
+  dbms_output.put_line('No. Ключевое слово');
+  dbms_output.put_line('--- -------------------------');
+
+  FOR ROW IN
+  (SELECT
+     ROWNUM,
+     keyword
+   FROM v$reserved_words
+   WHERE LENGTH(keyword) = word_length AND upper(keyword) LIKE upper(input_line || '%') AND reserved = 'Y')
+  LOOP
+    dbms_output.put_line(RPAD(row.rownum, 3) || ' ' || RPAD(row.keyword, 25));
+  END LOOP;
+END;
+/
+
